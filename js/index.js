b202b07ec83f87245676fd2448700beb2b02e311
Handell Desulme <handelldesulme@Handells-MacBook-Pro.local>
2019-12-03 19:00:19 -0500

    incomplete, but sets up index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,37 @@
 // Your code goes here
+
+/*
+let element = document.getORqueryByWhatever("element");
+element.addEventListener("event", () => {
+    console.log("random");
+});
+element.addEventListener("event", (e) => {
+    console.log(e.something);
+});
+mouseover
+keydown
+wheel
+drag / drop
+load
+focus
+resize
+scroll
+select
+dblclick
+*/
+
+/*
+Nest two similar events somewhere in the site and prevent the event propagation properly
+Stop the navigation items from refreshing the page by using preventDefault()
+*/
+
+const navItems = document.querySelectorAll("nav a");
+const headerImg = document.querySelector("header img");
+const headerParagrah = document.querySelector("header p"); //
+const imgContent = document.querySelectorAll(".img-content"); //flip on Y-axis
+const destinationHeader = document.querySelector(".content-destination h2");
+const buttons = document.querySelectorAll(".content-pick .btn"); //stop Prop here
+const ...;
+const ...;
+const ...;
+const ...;
